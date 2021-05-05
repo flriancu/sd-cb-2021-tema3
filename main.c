b6c5ed0d28ea2ef851e8e40807e6a59e9e3aa43f
flriancu <florineugen.iancu@gmail.com>
2021-05-05 18:39:10 +0300

    quick refactoring

--- main.c
+++ main.c
@@ -8,7 +8,9 @@
 
 typedef struct book_info_t
 {
-    int year;
+    char *author;
+    float rating;
+    int nb_pages;
 } book_info_t;
 
 
@@ -20,39 +22,83 @@ typedef struct book_trie_t
 } book_trie_t;
 
 
+#define MALLOC(p, sz)       do { (p) = malloc(sz); 0 && printf("* malloc %p %d\n", (p), (int)(sz)); } while ((void)0,0)
+#define CALLOC(p, n, sz)    do { (p) = calloc((n), (sz)); 0 && printf("* calloc %p %d*%d=%d\n", (p), (int)(n), (int)(sz), (int)(n*sz)); } while ((void)0,0)
+#define FREE(p)             do { free(p); 0 && printf("* free %p\n", (p)); } while ((void)0,0)
+
+
 void PrintBookInfo(const book_info_t *book_info, FILE *fo)
 {
-    fprintf(fo, "%d", book_info->year);
+    fprintf(fo, "[%s] [%d pg.] %.2f",
+        book_info->author,
+        book_info->nb_pages,
+        book_info->rating);
 }
 
 
-int GetAlphabetSize()
+const char * GetAlphabet()
 {
-    int sz = 0;
+    static const char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ- ";
 
-    sz += 'z' - 'a' + 1;
-    sz += 'Z' - 'A' + 1;
-    sz += strlen(".,;:-() ");
-
-    return sz;
+    return alphabet;
 }
 
 
-void AllocTrieNode(book_trie_t **t, const book_info_t *value)
+const int GetAlphabetSize()
+{
+    const char *alphabet = GetAlphabet();
+
+    return (int)strlen(alphabet);
+}
+
+
+int GetIndexOf(char c)
+{
+    const char *alphabet = GetAlphabet();
+    const int alphabet_sz = GetAlphabetSize();
+    int pos = 0;
+
+    for (int i = 0; i < alphabet_sz; ++i)
+    {
+        if (c == alphabet[i])
+        {
+            pos = i;
+            break;
+        }
+    }
+
+    return pos;
+}
+
+
+void AllocBookInfo(book_info_t **b, char *author, float rating, int nb_pages)
+{
+    MALLOC(*b, sizeof(book_info_t));
+    MALLOC((*b)->author, sizeof(char) * (strlen(author) + 1));
+
+    memcpy((*b)->author, author, sizeof(char) * (strlen(author) + 1));
+    (*b)->rating = rating;
+    (*b)->nb_pages = nb_pages;
+}
+
+
+void FreeBookInfo(book_info_t **b)
+{
+    FREE((*b)->author);
+    FREE(*b);
+    *b = NULL;
+}
+
+
+void AllocTrieNode(book_trie_t **t)
 {
     int alphabet_size = GetAlphabetSize();
 
-    *t = malloc(sizeof(book_trie_t));
+    MALLOC(*t, sizeof(book_trie_t));
 
     (*t)->is_word = 0;
     (*t)->value = NULL;
-    (*t)->children = calloc(alphabet_size, sizeof(book_trie_t *));
-
-    if (value)
-    {
-        (*t)->value = malloc(sizeof(book_info_t));
-        memcpy((*t)->value, value, sizeof(book_info_t));
-    }
+    CALLOC((*t)->children, alphabet_size, sizeof(book_trie_t *));
 }
 
 
@@ -60,15 +106,15 @@ void FreeTrieNode(book_trie_t **t)
 {
     if ((*t)->children)
     {
-        free((*t)->children);
+        FREE((*t)->children);
     }
 
     if ((*t)->value)
     {
-        free((*t)->value);
+        FreeBookInfo(&(*t)->value);
     }
 
-    free(*t);
+    FREE(*t);
     *t = NULL;
 }
 
@@ -92,7 +138,7 @@ void FreeTrie(book_trie_t **t)
 }
 
 
-void AddBook(book_trie_t *t, const char *key, const book_info_t *value)
+void AddBook(book_trie_t *t, const char *key, book_info_t *value)
 {
     int i;
     int len = strlen(key);
@@ -100,16 +146,17 @@ void AddBook(book_trie_t *t, const char *key, const book_info_t *value)
 
     for (i = 0; i < len; ++i)
     {
-        int index = key[i] - 'a';
+        int index = GetIndexOf(key[i]);
 
         if (node->children[index] == NULL)
         {
-            AllocTrieNode(&node->children[index], value);
+            AllocTrieNode(&node->children[index]);
         }
 
         node = node->children[index];
     }
 
+    node->value = value;
     node->is_word = 1;
 }
 
@@ -122,7 +169,7 @@ int SearchBook(book_trie_t *t, const char *key, book_info_t **out_value)
 
     for (i = 0; i < len; ++i)
     {
-        int index = key[i] - 'a';
+        int index = GetIndexOf(key[i]);
 
         if (node->children[index] == NULL)
         {
@@ -135,8 +182,7 @@ int SearchBook(book_trie_t *t, const char *key, book_info_t **out_value)
 
     if (node && node->is_word)
     {
-        *out_value = malloc(sizeof(book_info_t));
-        memcpy(*out_value, node->value, sizeof(book_info_t));
+        *out_value = node->value;
         return 1;
     }
 
@@ -147,46 +193,61 @@ int SearchBook(book_trie_t *t, const char *key, book_info_t **out_value)
 
 void Test(book_trie_t *books)
 {
-    char *keys[] = {
-        "lorelei",
-        "la lilieci",
-        "la medeleni",
-    };
+    FILE *fi;
+    const int k_buffer_len = 255;
+    char buffer[k_buffer_len];
 
-    book_info_t values[COUNT_OF(keys)] = {
-        { .year=1935 },
-        { .year=1973 },
-        { .year=1925 },
-    };
-
-    char *searches[] = {
-        "l", 
-        "la", 
-        "la ", 
-        "la medeleni", 
-        "lo",
-        "lorelei",
-        "la lilieci"
-    };
-
-    // -------------------------------------------------------
-
-    for (int i = 0; i < COUNT_OF(keys); ++i)
+    fi = fopen("commands.txt", "r");
+    if (fi == NULL)
     {
-        AddBook(books, keys[i], &values[i]);
+        return;
     }
 
-    for (int i = 0; i < COUNT_OF(searches); ++i)
+    while (fgets(buffer, k_buffer_len, fi))
     {
-        book_info_t *book_info;
-        int ret = SearchBook(books, searches[i], &book_info);
-        
-        printf("%20s: ", searches[i]);
-        ret ? PrintBookInfo(book_info, stdout) : printf("-");
-        printf("\n");
+        char *command = strtok(buffer, " ");
 
-        free(book_info);
+        if (0 == strcmp(command, "add_book"))
+        {
+            char *title = strtok(NULL, ":");
+            char *author = strtok(NULL, ":");
+            char *rating = strtok(NULL, ":");
+            char *nb_pages = strtok(NULL, "\n");
+
+            book_info_t *b;
+
+            AllocBookInfo(&b, author, (float)atof(rating), atoi(nb_pages));
+            AddBook(books, title, b);
+        }
+        else if (0 == strcmp(command, "search_book"))
+        {
+            char *search_term = strtok(NULL, "\n");
+            char last_char = search_term[strlen(search_term) - 1];
+
+            if (last_char == '~')
+            {
+                // TODO
+                search_term[strlen(search_term) - 1] = 0;
+            }
+            else
+            {
+                // TODO
+            }
+
+            book_info_t *book_info;
+            int ret = SearchBook(books, search_term, &book_info);
+            
+            printf("%20s: ", search_term);
+            ret ? PrintBookInfo(book_info, stdout) : printf("-");
+            printf("\n");
+        }
+        else if (0 == strcmp(command, "list_author"))
+        {
+            // TODO
+        }
     }
+
+    fclose(fi);
 }
 
 
@@ -194,7 +255,7 @@ int main()
 {
     book_trie_t *books;
 
-    AllocTrieNode(&books, NULL);
+    AllocTrieNode(&books);
 
     Test(books);
 
